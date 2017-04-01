953d15e58a68b35aaf9769a7552ef48737531f20
David Wang <dwang733@outlook.com>
2017-03-31 22:57:44 -0400

    Promisify updateTime() + minor refactoring

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /****************** WEBSITE/FOCUS TRACKING *********************/
 
 // Fires when new page is loaded
@@ -8,15 +10,16 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
 	if (details.frameId === 0) {
 		console.log("Loading new page!");
 		// Make sure we are actually seeing the new page 
-		// E.g. Open link in new tab will not trigger saveTime()
+		// E.g. Open link in new tab will not trigger updateTime()
 		chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-			console.log("Current tab: " + tab[0].url);
-			console.log("New page: " + details.url);
 			// Check if newly loaded page is actually on this tab
-			if (tab[0].url === details.url) {
+			if (tab.length > 0 && tab[0].url === details.url) {
 				console.log("Loaded on current tab!");
 				var tld = getTLD(details.url);
-				saveTime(tld);
+				updateTime(tld);
+			} else if (tab.length === 0) {
+				console.log("Loaded elsewhere in unfocused window!");
+				console.log("---------------------------------");
 			}
 			else {
 				console.log("Loaded elsewhere (new tab, etc.)!");
@@ -29,7 +32,7 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
 // Fires when user switches tab
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 	console.log("Switched tabs!");
-	updateCurrentTabTime();
+	updateWithCurrentTab();
 });
 
 // Fires when window changes focus (or no window is in focus)
@@ -37,83 +40,25 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
 	// If no windows are in focus, stop time tracking
 	if (windowId === chrome.windows.WINDOW_ID_NONE) {
 		console.log("Chrome unfocused!");
-		saveTime(undefined);
+		updateTime(undefined);
 	}
 	// Get active tab in newly focused window
 	else {
 		console.log("Chrome refocused!");
-		updateCurrentTabTime();
+		updateWithCurrentTab();
 	}
-});
-
-
-/****************** TIME TRACKING ******************/
-
-// Find previous URL visited and add time spent
-// Also set previous URL and time to current URL and time
-function saveTime(thisURL) {
-	chrome.storage.local.get(["prevStart", "prevURL"], function(getItems) {
-		console.log("New URL: " + thisURL);
-		console.log("Previous URL: " + getItems["prevURL"]);
-		var setObj = {};
-		var currTime = new Date().getTime();
-		var prevURL = getItems["prevURL"];
-		var prevStart = getItems["prevStart"];
-
-		chrome.storage.local.get(prevURL, function(item) {
-			if (prevURL !== undefined && prevStart !== undefined) {
-				var prevURLTime = item[prevURL] === undefined ? 0 : item[prevURL];
-				console.log("Total time spent on prev URL before: " + prevURLTime);
-				setObj[prevURL] = prevURLTime + (currTime - prevStart) / 1000;
-				console.log("Time just spent on prev URL: " + ((currTime - prevStart) / 1000));
-			}
-			setObj["prevStart"] = currTime;
-			setObj["prevURL"] = thisURL;
-			chrome.storage.local.set(setObj, function() {
-				// thisURL is undefined when we want to stop time tracking
-				// Manually remove "prevURL" and alarm
-				if (thisURL === undefined) {
-					chrome.storage.local.remove("prevURL", function() {
-						console.log("Dumping storage from no focus!");
-						printStorage();
-						console.log("Cleared alarm.");
-						chrome.alarms.clear("Update Time");
-					});
-				}
-				// Update time after certain interval
-				else {
-					console.log("Dumping storage from focus!");
-					printStorage();
-					chrome.alarms.create("Update Time", {"delayInMinutes": 1});
-				}
-			});
-		});
-	});
-}
-
-function updateCurrentTabTime() {
-	chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-		tld = getTLD(tab[0].url);
-		saveTime(tld);
-	});
-}
-
-// Update time when alarm called
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	console.log("Update from alarm at " + new Date().toLocaleTimeString());
-	updateCurrentTabTime();
 });
 
 // Detect if user is idle or not
 chrome.idle.onStateChanged.addListener(function(newState) {
 	console.log("Machine is " + newState);
 	if(newState === "active") {
-		updateCurrentTabTime();
+		updateWithCurrentTab();
 	}
 	// If idle or locked, stop time tracking
 	else {
-		saveTime(undefined);
-	}
+		updateTime(undefined);
+	}R
 });
 
 // Set time interval when user is active.
@@ -122,20 +67,96 @@ chrome.runtime.onInstalled.addListener(function(details) {
 });
 
 
+/****************** TIME TRACKING ******************/
+
+// Find previous URL visited and add time spent
+// Also set previous URL and time to current URL and time
+// Note that thisURL is undefined if we want to stop time tracking
+function updateTime(thisURL) {
+	safeMemoryOp(chrome.storage.local.get, ["prevStart", "prevURLStr"])
+		.then(function(prev) {
+			var prevStart = prev["prevStart"];
+			var prevURL = prev["prevURLStr"];
+			console.log("New URL: " + thisURL);
+			console.log("Prev URL: " + prevURL);
+			return Promise.all([prevStart, prevURL, safeMemoryOp(chrome.storage.local.get, prevURL)]);
+		}).then(function(results) {
+			var [prevStart, prevURL, prevURLTime] = results;
+			// If no previous time spent, set it to 0
+			prevURLTime = prevURLTime[prevURL] ? prevURLTime[prevURL] : 0;
+			var setObj = {};
+			var currTime = new Date().getTime();
+			// Only add time if there is a prev URL
+			if (prevStart && prevURL) {
+				console.log("Total time spent on prev URL before: " + prevURLTime);
+				setObj[prevURL] = prevURLTime + (currTime - prevStart) / 1000;
+				console.log("Time just spent on prev URL: " + ((currTime - prevStart) / 1000));
+			}
+			setObj["prevStart"] = currTime;
+			setObj["prevURLStr"] = thisURL;
+			console.log(setObj);
+			return safeMemoryOp(chrome.storage.local.set, setObj);
+		}).then(function() {
+			return thisURL ? true : safeMemoryOp(chrome.storage.local.remove, "prevURLStr");
+		}).then(function() {
+			// Keep tracking time
+			if (thisURL) {
+				console.log("Dumping storage from focus!");
+				printStorage();
+				chrome.alarms.create("Update Time", {"delayInMinutes": 1});
+			}
+			else {
+				console.log("Cleared alarm.")
+				chrome.alarms.clear("Update Time");
+				console.log("Dumping storage from no focus!");
+				printStorage();
+			}
+		}).catch(function(error) {
+			console.error(error);
+		});
+}
+
+// Update time when alarm called
+chrome.alarms.onAlarm.addListener(function(alarm) {
+	console.log("Update from alarm at " + new Date().toLocaleTimeString());
+	updateWithCurrentTab();
+});
+
+
 /**************** HELPER FUNCTIONS ******************/
 
 // Get top level domain name
-// E.g. https://developer.chrome.com/extensions/storage -> https://developer.chrome.com
+// E.g. https://developer.chrome.com/extensions/ -> https://developer.chrome.com
 function getTLD(thisURL) {
-	regex = /^(\w+:\/\/[^\/]+).*$/;
+	var regex = /^(\w+:\/\/[^\/]+).*$/;
 	return thisURL.match(regex)[1];
 }
 
 function printStorage() {
 	chrome.storage.local.get(null, function(items) {
-		for (i in items) {
+		for (var i in items) {
 			console.log("Key: " + i + "		Value: " + items[i]);
 		}
 		console.log("---------------------------------");
+	});
+}
+
+// Pass URL in current tab to updateTime()
+function updateWithCurrentTab() {
+	chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+		var tld = getTLD(tab[0].url);
+		updateTime(tld);
+	});
+}
+
+// Performs the memory op with the given arg and checks that it succeeded
+function safeMemoryOp(op, arg) {
+	return new Promise(function(resolve, reject) {
+		op(arg, function(items) {
+			if (chrome.runtime.lastError) {
+				reject(Error("Memory error"));
+			}
+			resolve(items);
+		});
 	});
 }
